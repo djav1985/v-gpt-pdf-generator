d805649113e8ab609de350d70080c65cd8b72cd8
vontainment <services@vontainment.com>
2024-05-08 03:59:41 -0400

    Update functions.py

--- app/functions.py
+++ app/functions.py
@@ -1,6 +1,7 @@
 # Importing required libraries and modules
 import os
-import aiohttp
+import asyncio
+import aiofiles.os
 from pathlib import Path
 from datetime import datetime, timedelta
 from weasyprint import HTML, CSS
@@ -17,24 +18,32 @@ async def generate_pdf(html_content: str, css_content: str, output_path: Path):
                 string="body { font-family: 'Arial', sans-serif; } h1 { color: #66cc33; } h2, h3, h4, h5, h6 { color: #4b5161; } p { margin: 0.5em 0; } a { color: #0366d6; text-decoration: none; }"
             )
         )
-        HTML(string=html_content).write_pdf(target=output_path, stylesheets=[css])
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(
+            None,
+            lambda: HTML(string=html_content).write_pdf(
+                target=output_path, stylesheets=[css]
+            ),
+        )
     except Exception as e:
         print(f"Error generating PDF: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error generating PDF: {str(e)}")
 
 
 # Function to clean up the downloads folder
-def cleanup_downloads_folder(folder_path: str):
+async def cleanup_downloads_folder(folder_path: str):
     try:
         now = datetime.now()
         age_limit = now - timedelta(days=7)
-        for filename in os.listdir(folder_path):
+        filenames = await asyncio.to_thread(os.listdir, folder_path)
+        for filename in filenames:
             file_path = os.path.join(folder_path, filename)
-            if (
-                os.path.isfile(file_path)
-                and datetime.fromtimestamp(os.path.getmtime(file_path)) < age_limit
-            ):
-                os.remove(file_path)
+            if await aiofiles.os.path.isfile(file_path):
+                file_mod_time = datetime.fromtimestamp(
+                    await asyncio.to_thread(os.path.getmtime, file_path)
+                )
+                if file_mod_time < age_limit:
+                    await asyncio.to_thread(os.remove, file_path)
     except Exception as e:
         print(f"Cleanup error: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Cleanup error: {str(e)}")
