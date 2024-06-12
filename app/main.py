9ab66e7f1e2b15a42a306c2b00e20152953308d0
vontainment <services@vontainment.com>
2024-06-12 15:39:17 -0400

    new file:   .dockerignore 	new file:   .gitea/workflows/main.yml 	modified:   Dockerfile 	renamed:    app/functions.py -> app/dependencies.py 	modified:   app/main.py 	deleted:    app/public/brushed-alum.png 	deleted:    app/public/favicon.ico 	modified:   app/public/index.html 	deleted:    app/public/profile.png 	modified:   docker-compose.yml 	renamed:    app/requirements.txt -> requirements.txt

--- app/main.py
+++ app/main.py
@@ -13,7 +13,8 @@ app = FastAPI(
     title="PDF Generation API",
     version="0.1.0",
     description="A FastAPI application that generates PDFs from HTML and CSS content",
-    servers=[{"url": os.getenv('BASE_URL'), "description": "Base API server"}],
+    root_path=os.getenv("ROOT_PATH", ""),
+    servers=[{"url": os.getenv("BASE_URL", ""), "description": "Base API server"}],
 )
 
 # Including Routers for different endpoints
@@ -24,6 +25,3 @@ app.include_router(root_router)
 
 # Mount a static files directory at /downloads to serve generated PDFs
 app.mount("/downloads", StaticFiles(directory="/app/downloads"), name="downloads")
-
-# Serve static files (HTML, CSS, JS, images)
-app.mount("/static", StaticFiles(directory="/app/public"), name="static")
